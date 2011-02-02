d8802bda1358905d4a22a437cf0228d0e2c33c23
MiloFoxburr <MiloFoxburr@489d5ef2-b889-4d9c-fd3c-e0d20f979887>
2011-02-02 16:02:09 +0000

    Durability warning should no longer spam. Now repeats every 2 and a half minutes. Also displays on Screen and in Chat box to ensure those who hide chat don't miss it. On screen prompt likely needs moving and is untested.

--- trunk/MirClient/ClMain.pas
+++ trunk/MirClient/ClMain.pas
@@ -13231,13 +13231,17 @@ var
   sHint: string;
 begin
   if g_Config.boDuraWarning and (g_MySelf <> nil) { and (not g_MySelf.m_boDeath)} then begin
-    if GetTickCount - g_dwHintItemDuraTick > 1000 * 2 then begin
+    if GetTickCount - g_dwHintItemDuraTick > 1000 * 150 then begin
       g_dwHintItemDuraTick := GetTickCount;
       for I := U_DRESS to U_CHARM do begin
         if g_UseItems[I].S.Name <> '' then begin
           if g_UseItems[I].Dura <= Round(g_UseItems[I].DuraMax * 10 / 100) then begin
-            sHint := g_UseItems[I].S.Name + ' durability is low!';
-            DScreen.AddChatBoardString(sHint, clyellow, clRed);
+            sHint := g_UseItems[I].S.Name + ' durability is low! Your item may break if Durability reaches 0';
+
+            //if not PlayScene.EdChat.Visible then begin
+              DScreen.AddSysMsg(sHint, SCREENWIDTH - 150, 450, clYellow);
+            //else
+               DScreen.AddChatBoardString(sHint, clyellow, clRed);
           end;
         end;
       end;
